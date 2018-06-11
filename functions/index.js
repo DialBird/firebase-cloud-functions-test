ef409d1720196abb0962dde47b40eeb93a31a5be
keisuke <rememberthatk@icloud.com>
2018-06-11 11:05:46 +0900

    Edit index.js and Add CreateCard functions

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,24 @@
 const functions = require('firebase-functions');
 
-const admin = require('firebase-admin');
-admin.initializeApp();
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
 exports.hello = functions.https.onRequest((req, res) => {
   const name = req.query.name;
   res.send(`Hello ${name}!!`)
 });
 
-exports.addMessage = functions.https.onRequest((req, res) => {
-  const original = req.query.text;
-  return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
-    return res.redirect(303, snapshot.ref.toString());
+exports.c_card = functions.https.onRequest((req, res) => {
+  const stripe = require('stripe')(functions.config().stripe.sk_test);
+  stripe.tokens.create({
+    card: {
+      "number": '4242424242424242',
+      "exp_month": 12,
+      "exp_year": 2019,
+      "cvc": '123'
+    }
+  }, (err, token) => {
+    if (err !== null) {
+      res.send(err);
+    } else {
+      res.send(token)
+    }
   });
 });
