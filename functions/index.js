50e4e1d47921ee4a8fe408758901e256f34a4d5d
keisuke <rememberthatk@icloud.com>
2018-06-11 19:22:05 +0900

    fix index.js

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,26 @@
 const functions = require('firebase-functions');
 const stripe = require('stripe')(functions.config().stripe.sk_test);
 
-exports.hello = functions.https.onRequest((req, res) => {
-  const name = req.query.name;
-  res.send(`Hello ${name}!!`)
-});
-
-exports.c_card = functions.https.onRequest((req, res) => {
+exports.c_customer = functions.https.onRequest((req, res) => {
   const number = req.body.number;
   const exp_month = req.body.exp_month;
   const exp_year = req.body.exp_year;
   const cvc = req.body.cvc;
 
-  stripe.tokens.create({
-    card: {
+  stripe.customers.create({
+    description: 'Customer for mason.jackson@example.com',
+    email: 'example@co.jp',
+    source: {
+      object: 'card',
+      currency: 'jpy',
       number,
       exp_month,
       exp_year,
       cvc,
-    }
-  }, (err, token) => {
-    if (err !== null) {
-      res.send(err);
-    } else {
-      res.send(token)
-    }
+    },
+  }).then(customer => {
+    res.send(customer);
+  }).catch(err => {
+    res.send(err);
   });
 });
